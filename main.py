cf47c7fb0df3b8d190ca02e7441edb5b64808e58
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-30 16:20:45 +0500

    Decorators @classmethod and @staticmethod

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 class player: #Class
     membership = True #Class Attribute
-    def __init__(self, name, age): #Constructor
+    def __init__(self, name='Tayyab', age=20): #Constructor
         if(player.membership):
             self.name = name #Attribute
             self.age = age #Attribute
     
     def info(self): #Method
         print(f'Player {self.name} has age of {self.age}')
+    @classmethod #Decorator we can access class attribute using cls keyword
+    def adding_things(cls, num1, num2):
+        return cls(num1 + num2)
 
-player1 = player("Tayyab", 20) #Object
+    @staticmethod
+    def add_things2(num1, num2):
+        return num1 + num2
 
-player1.info() #Method
+player3 = player.adding_things(2, 3) #@classmethod
+player4 = player.add_things2(2, 3) #@staticmethod
+print(player3.age)
+print(player4)
+
+# player1 = player("Tayyab", 20) #Object
+#
+# player1.info() #Method
