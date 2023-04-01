b4af046be18e372dafd3b6aeffc123168fe434a9
Tayyab <tayyab.shahzad100000@gmail.com>
2023-04-01 20:47:53 +0500

    Abstraction

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-#Encapsulation
-class player: #Class
-    membership = True #Class Attribute
-    def __init__(self, name='Tayyab', age=20): #Constructor
-        if(player.membership):
-            self.name = name #Attribute
-            self.age = age #Attribute
+#Abstraction
+# class player: #Class
+#     membership = True #Class Attribute
+#     def __init__(self, name='Tayyab', age=20): #Constructor
+#         if(player.membership):
+#             self.name = name #Attribute
+#             self.age = age #Attribute
 
-    def info(self): #Method
-        print(f'Player {self.name} has age of {self.age}')
+#     def info(self): #Method
+#         print(f'Player {self.name} has age of {self.age}')
 
 
+tup1 = (1,2,3,1,1,4,5,6)
 
-player1= player()
-
-player1.info() #Info is the encapsulated method of player class
-
-'Hello World'.capitalize() #Capitalize is the encapsulated method of string class
+print(tup1.count(1)) #.count working is abstracted form us
