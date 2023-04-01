e449b9177562bcbc4d92f0eb2c8fa30bdca23067
Tayyab <tayyab.shahzad100000@gmail.com>
2023-04-01 21:19:59 +0500

    Inheritance Basics Part 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Inheritance
+#Polymorphism
 class User():
     def Sign_In(self):
         print('Logged In')
@@ -10,3 +10,11 @@ class Archer(User):
 
 Archer1 = Archer()
 Archer1.Sign_In()
+
+#isinstance(instance , class)
+
+print(isinstance(Archer1, Archer))
+
+print(isinstance(Archer1, User))
+
+print(isinstance(Archer1, object)) # Object by default is parent class for all the class in python
